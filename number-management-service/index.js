0048fca56bc7bb964eb7495476ca8af7a56f1ee0
Nishant Karki <116018595+NishnathFreecss@users.noreply.github.com>
2023-07-15 10:24:17 +0530

    Update index.js
    added some instructions

--- number-management-service/index.js
+++ number-management-service/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const axios = require('axios');
-
+//Setting up the Express application
 const app = express();
 app.use(express.json());
-
+//Defining the route
 app.get('/numbers', async (req, res) => {
   const urls = req.query.url;
 
   if (!urls || !Array.isArray(urls)) {
     return res.status(400).json({ error: 'Invalid URL parameter' });
   }
-
+  //Fetching data from the provided URLs
   try {
     const promises = urls.map(url => axios.get(url));
     const responses = await Promise.allSettled(promises);
-
+    //Processing the responses and merging numbers
     let mergedNumbers = [];
     
     responses.forEach(response => {
